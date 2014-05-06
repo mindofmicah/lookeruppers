9764f78e587e8693704bd2b6a8fd4cbdd746e628
michael eschbacher <michaeleschbacher@gmail.com>
2014-05-05 20:04:23 -0500

    initial commit

---
+++ LookerUppers/Google/BookLookup.php
@@ -0,0 +1,21 @@
+<?php
+namespace Whamdonk\LookerUppers\Google;
+
+/**
+ * 
+ **/
+class BookLookup
+{
+    public function lookupByISBN($isbn)
+    {
+   $response = json_decode(file_get_contents('https://www.googleapis.com/books/v1/volumes?q=' . $isbn));
+    if ($response->totalItems == 0) {
+        return null;
+    }
+    return $response->items[0]->volumeInfo;
+
+   //     $response = file_get_contents('https://www.googleapis.com/books/v1/volumes?q=isbn:' . $isbn);
+echo '<pre>';
+        dd($response);
+    }
+}
